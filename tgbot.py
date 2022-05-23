7ea502eb8550b29d64f48fec82df2475eefaa57c
oleksmin <alxminchuk@gmail.com>
2022-05-24 01:23:16 +0300

    after errors fixed

--- tgbot.py
+++ tgbot.py
@@ -1,22 +1,35 @@
 import re  # Regular Expressions
 import nltk # Natural Language Toolkit
-import random # Random
+import random, psutil
+from numpy import append # Random and pseudorandom data for seeding
 
 
 BOT_CONFIG = {
     # Все намерения которые поддерживает наш бот
    "intents": {
         "hello": {
-            "examples" : ["Привет", "Здарова", "Йо", "Приветос", "Хеллоу"],
+            "examples" : ["Привет", "Здарова", "Йо", "Хай", "Хеллоу"],
             "responses": ["Здравстсвтсвтвтвуй человек", "И тебе не хворать", "Здоровее видали"],
         },
         "how_are_you": {
             "examples" : ["Как дела", "Чо каво", "Как поживаешь"],
             "responses": ["Маюсь Фигней", "Веду интенсивы", "Учу Пайтон"],
+        },
+        "chatting1": {
+            "examples" : ["Что нового", "Чем занимаешься", "Какие планы"],
+            "responses": ["Продолжу фигнёй страдать", "Скоро спать пойду", "Пойду гулять"],
+        },
+        "acks": {
+            "examples" : ["Ладно", "Хорошо", "Отлично","Давай","Конечно", "Ясно","Договорились"],
+            "responses": ["А теперь твой вопрос", "Ок, теперь тебя слушаю", "Ок. Я слушаю"],
+        },
+        "stopper":{
+            "examples" : ["Пока!", "До свиданья", "Хватит", "Закончили", "Стоп", "Иди нахер", "Отвали"],
+            "responses": ["Ладно, до встречи!", "Так и быть...", "И тебе всего хорошего!", "Жаль, что так рано закончили", "Ещё увидимся!"],
         }
     },
     # Фразы когда бот не может ответить
-    "failure_phrases": ["Даже не знаю что сказать", "Поставлен в тупик", "Перефразируйте, я всего лишь бот"],
+    "failure_phrases": ["Чоэтобыло?", "Даже не знаю что сказать", "Поставлен в тупик", "Перефразируйте, я всего лишь бот"],
 }
 
 
@@ -57,19 +70,38 @@ def getAnswer(text, examples, responses):
    
 
 def main():
-    print("Тупе техт хере:")
-    text = input()
-
-    # Для каждого намерения, пытаемся понять ответ
-    answer = ''
-    for intent in BOT_CONFIG["intents"].values():
-        phrase = getAnswer(text, intent["examples"], intent["responses"])
-        if (answer): answer = answer + "\n" + phrase  # Выводим на экран случайный элемент списка responses
-        else: answer = phrase
+    # initialize random seed
+    random.seed(int(psutil.virtual_memory()[3]%psutil.virtual_memory()[4]*psutil.cpu_percent(1)))
     
-    if (answer.length() < 1)
-        answer = random.choice(BOT_CONFIG["failure_phrases"])
+    intents_noted = set()
+    
+    # Для каждого намерения, пытаемся понять ответ
+    print("Привет! Давай поговорим?")
 
+    while True:
+        text = input()
+
+        answer = ''
+
+        for int_name, intent in BOT_CONFIG["intents"].items():
+            phrase = getAnswer(text, intent["examples"], intent["responses"])
+            if (phrase): # если что-то нашлось
+                if (answer): 
+                    answer = answer + "\n" + phrase  # дописываем в наш ответ
+                else: 
+                    answer = phrase # или просто записываем в ответ
+                intents_noted.add(int_name) # запоминаем, какие намерения встретились
+        
+        if (not answer): # если бот вообще ничего не понял из сказанного
+            answer = random.choice(BOT_CONFIG["failure_phrases"]) # выдать дежурную фразу
+
+        print(answer)
+
+        if ("stopper" in intents_noted): # если кожаный ублюдок хочет закончить разговор
+            print("Было приятно пообщаться!") # заканчиваем
+            break
+        else: 
+            intents_noted = set() # в противном случае можно обнулить намерения и продолжаем
 
 
 if __name__ == "__main__":
